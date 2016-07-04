115cb6e2875d1c0ec5d5419880943a8f23fed643
George G <ggogarten@gmail.com>
2016-07-04 12:28:36 -0400

    initial layout and outlets ready

---
+++ trak3/ActivityViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ActivityViewController.swift
+//  trak3
+//
+//  Created by George Gogarten on 7/4/16.
+//  Copyright © 2016 George Gogarten. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ActivityViewController: UIViewController {
+    
+    
+    @IBOutlet weak var unitsLabel: UILabel!
+    
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    
+    @IBOutlet weak var speedOutputLabel: UILabel!
+    
+    
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    
+    @IBOutlet weak var distanceOutputLabel: UILabel!
+    
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
+    @IBOutlet weak var timeOutputLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    
+    @IBOutlet weak var pauseButton: UIButton!
+    
+    
+    @IBOutlet weak var endButton: UIButton!
+    
+
+    @IBOutlet weak var map: MKMapView!
+    
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
