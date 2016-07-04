a232505e0ffa1662d26849fc838a0cef65124f37
George G <ggogarten@gmail.com>
2016-07-04 17:41:09 -0400

    working map, and labels!!!!!

--- trak3/ActivityViewController.swift
+++ trak3/ActivityViewController.swift
@@ -22,13 +22,16 @@ class ActivityViewController: UIViewController, MKMapViewDelegate, CLLocationMan
     
     var units = String("Metric")
     
+    var pausedState = false
+    
+    var activityLive = false
+    
     var locations = [CLLocation]()
     
     
     @IBOutlet weak var map: MKMapView!
     
     var locationManager = CLLocationManager()
-    
     
     @IBOutlet weak var unitsLabel: UILabel!
     
@@ -49,33 +52,89 @@ class ActivityViewController: UIViewController, MKMapViewDelegate, CLLocationMan
     
     @IBOutlet weak var timeOutputLabel: UILabel!
     
-    func increaseTimer(){
+    
+    @IBOutlet weak var startActivityButtonLabel: UIButton!
+    
+    
+    @IBOutlet weak var unitSwitch: UISwitch!
+    
+    
+    
+    @IBAction func unitSwitchTrigger(sender: AnyObject) {
         
-        time = time + 1
-        
-        if time > 3600 {
+        if unitSwitch.on == true {
             
-            let hours = Int(time) / 3600
-            let minutes = Int(time) / 60 % 60
-            let seconds = Int(time) % 60
+            print("Switch On")
+            units = "Metric"
+            unitsLabel.text = String("Metric")
+            speedLabel.text = String("Speed (Kph)")
             
-            //            let secondsQuantity = HKQuantity(unit: HKUnit.secondUnit(), doubleValue: time)
-            //            let distanceQuantity = HKQuantity(unit: HKUnit.meterUnit(), doubleValue: distance)
-            
-            timeOutputLabel.text = String(format:"%02i:%02i:%02i", hours, minutes, seconds)
-           
             
         } else {
             
-            let minutes = Int(time) / 60 % 60
-            let seconds = Int(time) % 60
+            print("Switch Off")
+            units = "Imperial"
+            unitsLabel.text = String("Imperial")
+            speedLabel.text = String("Speed (Mph)")
+        }
+    }
+    
+    
+    
+    
+    func increaseTimerandDistance(){
+        
+        unitsLabel.text = units
+        
+        time = time + 1
+        
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        let meters = self.distance
+        let yards = meters * 1.09361
+        let miles = meters * 0.000621371
+        let kilometers = meters / 1000
+        
+        
+        if time > 3600 {
+            
+            timeOutputLabel.text = String(format:"%02i:%02i:%02i", hours, minutes, seconds)
+            
+        } else {
             
             timeOutputLabel.text = String(format:"%02i:%02i", minutes, seconds)
             
         }
         
-        
-        
+        if units == "Metric" {
+            
+            if meters > 1000 {
+                
+                distanceOutputLabel.text = String(format:"%00.2f", kilometers)
+                distanceLabel.text = String("Distance (Km)")
+                
+            } else {
+                
+                distanceOutputLabel.text = String(format:"%3.f", meters)
+                distanceLabel.text = String("Distance (Meters)")
+                
+            }
+            
+        } else {
+            
+            if yards > 1000 {
+                
+                distanceOutputLabel.text = String(format:"%00.2f", miles)
+                distanceLabel.text = String("Distance (Miles)")
+                
+            } else {
+                
+                distanceOutputLabel.text = String(format:"%3.f", yards)
+                distanceLabel.text = String("Distance (Yards)")
+                
+            }
+        }
     }
     
     
@@ -83,7 +142,36 @@ class ActivityViewController: UIViewController, MKMapViewDelegate, CLLocationMan
     
     @IBAction func startActivityButton(sender: AnyObject) {
         
-        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ActivityViewController.increaseTimer), userInfo: nil, repeats: true)
+        if pausedState == true {
+            
+            locationManager.startUpdatingLocation()
+            
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ActivityViewController.increaseTimerandDistance), userInfo: nil, repeats: true)
+            
+            activityLive = true
+            
+        } else {
+            
+            if activityLive == true {
+                
+                locationManager.startUpdatingLocation()
+                
+                timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ActivityViewController.increaseTimerandDistance), userInfo: nil, repeats: true)
+                
+                
+            } else {
+                
+                self.distance = 0.0
+                
+                locationManager.startUpdatingLocation()
+                
+                timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ActivityViewController.increaseTimerandDistance), userInfo: nil, repeats: true)
+                
+                activityLive = true
+                
+            }
+            
+        }
         
     }
     
@@ -91,6 +179,12 @@ class ActivityViewController: UIViewController, MKMapViewDelegate, CLLocationMan
     @IBAction func pauseActivityButton(sender: AnyObject) {
         
         timer.invalidate()
+        
+        pausedState = true
+        
+        startActivityButtonLabel.setTitle("Go", forState: UIControlState.Normal)
+        
+        locationManager.stopUpdatingLocation()
         
     }
     
@@ -100,6 +194,9 @@ class ActivityViewController: UIViewController, MKMapViewDelegate, CLLocationMan
         timer.invalidate()
         time = 0
         timeOutputLabel.text = "00:00"
+        distanceOutputLabel.text = "0"
+        pausedState = false
+        activityLive == false
         
     }
     
@@ -125,49 +222,45 @@ class ActivityViewController: UIViewController, MKMapViewDelegate, CLLocationMan
         self.map.mapType = MKMapType(rawValue: 0)!
         self.map.userTrackingMode = MKUserTrackingMode(rawValue: 2)!
         
+        unitSwitchTrigger(true)
+        
         
         // Do any additional setup after loading the view.
     }
     
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    func locationManager(manager: CLLocationManager!, didUpdateToLocation newLocation: CLLocation!, fromLocation oldLocation: CLLocation!){
         
-        //        print(locations)
-        
-        
-        
-        for location in locations {
+        if oldLocation != nil {
+            
+            distance = distance + newLocation.distanceFromLocation(oldLocation)
+            
+            print(distance)
+            
+            if units == "Metric" {
                 
-                if locations.count > 0 {
-                    
-                    print(locations)
-                    distance = distance + location.distanceFromLocation(locations[0])
-                    print(distance)
-
-                    if units == "Metric" {
-                        
-                        var speed = location.speed
-                        var speedKph = String(format: "%.2f", speed * 3.6)
-                        
-                        self.speedOutputLabel.text = "\(speedKph)"
-                        
-                    } else {}
-
-                    
-                } else {
-                    
-                    self.locations.append(locations[0])
-                    
-                }
+                var speed = newLocation.speed
+                var speedKph = String(format: "%.2f", speed * 3.6)
+                
+                self.speedOutputLabel.text = "\(speedKph)"
+                
+            } else {
+                
+                var speed = newLocation.speed
+                var speedMph = String(format: "%.2f", speed * 2.2369363)
+                
+                self.speedOutputLabel.text = "\(speedMph)"
+                
+                
+                
             }
         }
-        
-        
-        
-        
-        //        print("present location : \(newLocation.coordinate.latitude), \(newLocation.coordinate.longitude)")
-        //        print("speed : \(newLocation.speed)")
-        
-        
+    }
+    
+    
+    //        print("present location : \(newLocation.coordinate.latitude), \(newLocation.coordinate.longitude)")
+    //        print("speed : \(newLocation.speed)")
+    
+    
     
     
     
